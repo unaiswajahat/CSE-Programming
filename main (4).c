0f60d986e45ebaeba888ac72bcffa5105a6f0aa9
unaiswajahat <95277894+unaiswajahat@users.noreply.github.com>
2021-11-30 17:22:32 +0530

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int i, j, rows;
+    printf("Enter the number of rows:");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; ++i) {
+       for (j = 1; j <= i; ++j) {
+           printf("* ");
+       }   
+        printf("\n");
+    } 
+
+    return 0;
+}
