36ff4dff6cb45eab2b7d567a7d3e27c16d5db831
unaiswajahat <95277894+unaiswajahat@users.noreply.github.com>
2021-11-30 16:01:48 +0530

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{int i;
+      printf("Even numbers between 1 to 50 (inclusive):\n");
+      for (i = 1; i <= 50; i++) 
+      {
+         
+              if(1%2 == 0)
+              {
+                printf("%d ", i);
+              }
+              
+      }       
+    return 0;
+}
